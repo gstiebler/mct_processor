6f71d00b258cfe4f031f13aaca8f8b03c33a9f3d
Guilherme Stiebler <guilhermemst@gmail.com>
2014-09-25 20:57:52 -0300

    eh linha nome

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def ehLinhaRota(line):
     else:
         return False
     return True
+    
+    
+def ehLinhaNome(line):
+    strings = line.split(" ")
+    if len(strings) != 2:
+        return False
+    if len(strings[0]) < 3 or len(strings[1]) < 3:
+        return False
+    if not strings[0].isdigit():
+        return False
+        
+    return True
 
 
 file = open("output.txt", "w")
@@ -19,6 +31,8 @@ file = open("output.txt", "w")
 for line in fileinput.input():
     if ehLinhaRota(line):
         file.write("### LINHA ROTA ### ")
+    if ehLinhaNome(line):
+        file.write("+++ LINHA NOME +++ ")
     file.write(line)
 
 
