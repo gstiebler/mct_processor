a9a6625bd9786a9abffee3954e24aa75c61cd874
Guilherme Stiebler <guilhermemst@gmail.com>
2014-09-25 23:21:52 -0300

    separando nome e rota

--- main.py
+++ main.py
@@ -10,24 +10,30 @@ class MyHTMLParser(HTMLParser):
         self.fileDebug = fileDebug
         self.tagDepth = 0
         self.lastAttr = ""
+        self.insideBold = False
 
     def handle_starttag(self, tag, attrs):
         self.tagDepth = self.tagDepth + 1
         self.fileDebug.write("Start tag: {0}, tag depth: {1}\n".format(tag, self.tagDepth))
+        if tag == "b":
+            self.insideBold = True
         for attr in attrs:
             self.fileDebug.write("     attr: |{0}|\n".format(attr))
             self.lastAttr = attr
     def handle_endtag(self, tag):
         self.tagDepth = self.tagDepth - 1
+        if tag == "b":
+            self.insideBold = False
         self.fileDebug.write( "End tag  : {0}, tag depth: {1}\n".format(tag, self.tagDepth))
     def handle_data(self, data):
         self.fileDebug.write( "Valor  : %s\n" % (data))
         nameAttr = "stylefont-size:7.0pt;font-family:\"Times New Roman\",\"serif\""
         lastAttrStr = ''.join(self.lastAttr)
         if lastAttrStr == nameAttr:
-            self.outputFile.write("Nome rota: {0}\n".format(data))
-        else:
-            self.fileDebug.write( "lastAttr\n{0}\n{1}\n".format(len(lastAttrStr), lastAttrStr))
+            if self.insideBold:
+                self.outputFile.write("Nome rota: {0}\n".format(data))
+            else:
+                self.outputFile.write("### Rota: {0}\n".format(data))
     def handle_comment(self, data):
         self.fileDebug.write( "Comment  : %s\n" % (data))
     def handle_entityref(self, name):
