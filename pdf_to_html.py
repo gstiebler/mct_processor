a09c222fe7ba7fd8d956f2f5a930a3496dbfa007
Guilherme Stiebler <guilhermemst@gmail.com>
2014-10-01 20:41:53 -0300

    convertendo arquivos html de saída

--- pdf_to_html.py
+++ pdf_to_html.py
@@ -1,16 +1,23 @@
 import os
+import shutil
 
 currPath = os.path.dirname(os.path.realpath(__file__))
 PDFsDir = currPath + '\\PDFs'
 HTMLDir = currPath + '\\saida_html' 
 lenPDFsDir = len(PDFsDir)
+pdf2htmlExe = "pdftohtml.exe"
 
+count = 0
 for currDir, dirs, files in os.walk(PDFsDir):
-    print "files {}".format(files)
-        
-    print "subdirs {}".format(currDir)
-    
     baseDir = currDir[lenPDFsDir:]
     newHTMLDir = HTMLDir + baseDir
     if not os.path.exists(newHTMLDir):
         os.makedirs(newHTMLDir)
+        
+    for file in files:
+        fileAndPath = "{}\\{}".format(currDir, file)
+        outputDir = "{}\\{}".format(newHTMLDir, file[:-4])
+        command = "{} {} {}".format(pdf2htmlExe, fileAndPath, outputDir)
+        count = count + 1
+        print "{} - Convertendo arquivo {}...".format(count, fileAndPath)
+        os.system(command)
